1c099498c21b79c5761f3e298e59ae190ad5e364
grantfirl <grantf@ucar.edu>
2021-05-25 14:14:10 -0600

    add ZM deep convection files (incomplete)

---
+++ physics/iap_state_update.F90
@@ -0,0 +1,503 @@
+module iap_state_update
+  
+  use shr_kind_mod, only: r8=>shr_kind_r8
+  
+  public
+  
+  implicit none
+  
+  contains
+  
+  subroutine physics_update(lu, lv, ls, lq, fv_dycore, name, microp_scheme, top, bot, ncol, pcols, pver, pcnst, ixcldice, ixcldliq, ixnumice, ixnumliq, rair, gravit, cpair, zvir, dt, qmin, tend_u, tend_v, tend_s, tend_q, lnpint, lnpmid, pint, pmid, pdel, rpdel, phis, state_u, state_v, state_s, state_q, t, zm, zi)
+    logical, intent(in) :: lu, lv, ls, fv_dycore
+    logical, intent(in), dimension(:) :: lq
+    character*24, intent(in) :: name    ! name of parameterization which produced tendencies.
+    character(len=16), intent(in) :: microp_scheme
+    integer, intent(in) :: top, bot, ncol, pcols, pver, pcnst
+    integer, intent(in) :: ixcldice, ixcldliq                  ! indices for CLDICE and CLDLIQ
+    integer, intent(in) :: ixnumice, ixnumliq                  ! indices for ice and liquid number concentrations
+    real(kind=r8), intent(in   ),                :: rair, gravit, cpair, zvir
+    
+    real(kind=r8), intent(in   ),                :: dt
+    real(kind=r8), intent(in   ), dimension(:)   :: qmin
+    real(kind=r8), intent(in   ), dimension(:,:) :: tend_u, tend_v, tend_s
+    real(kind=r8), intent(in   ), dimension(:,:,:) :: tend_q
+    
+    real(kind=r8), intent(in   ), dimension(:,:) :: lnpint, pint
+    real(kind=r8), intent(in   ), dimension(:,:) :: lnpmid, pmid, pdel, rpdel
+    real(kind=r8), intent(in   ), dimension(:)   :: phis
+    
+    real(kind=r8), intent(inout), dimension(:,:) :: state_u, state_v, state_s
+    real(kind=r8), intent(inout), dimension(:,:,:) :: state_q
+    real(kind=r8), intent(inout), dimension(:,:) :: t, zm
+    real(kind=r8), intent(inout), dimension(:,:) :: zi
+    
+    integer :: i,k,m
+    
+    ! Update u,v fields
+    if(lu) then
+       do k = top, bot
+          do i = 1, ncol
+             state_u(i,k) = state_u(i,k) + tend_u(i,k) * dt
+          end do
+       end do
+    end if
+
+    if(lv) then
+       do k = top, bot
+          do i = 1, ncol
+             state_v(i,k) = state_v(i,k) + tend_v(i,k) * dt
+          end do
+       end do
+    end if
+    
+    ! Update dry static energy
+    if(ls) then
+       do k = top, bot
+          do i = 1, ncol
+             state_s(i,k) = state_s(i,k) + tend_s(i,k) * dt
+          end do
+       end do
+    end if
+    
+    do m = 1, pcnst
+       if(lq(m)) then
+          do k = top, bot
+             do i = 1,ncol
+                state_q(i,k,m) = state_q(i,k,m) + tend_q(i,k,m) * dt
+             end do
+          end do
+
+          ! now test for mixing ratios which are too small
+          ! don't call qneg3 for number concentration variables
+          if (m .ne. ixnumice  .and.  m .ne. ixnumliq) then
+            call qneg3(ncol, pcols, pver, m, m, qmin(m), state_q(1,1,m))
+          else
+             do k = top, bot
+                do i = 1,ncol
+                   ! checks for number concentration
+                   state_q(i,k,m) = max(1.e-12_r8,state_q(i,k,m))
+                   state_q(i,k,m) = min(1.e10_r8,state_q(i,k,m))
+                end do
+             end do
+          end if
+       end if
+    end do
+    
+    ! special tests for cloud liquid
+    if (ixcldliq > 1) then
+       if(lq(ixcldliq)) then
+          if (name == 'stratiform' .or. name == 'cldwat'  ) then
+  #ifdef PERGRO
+             where (state_q(:ncol,:pver,ixcldliq) < 1.e-12_r8)
+                state_q(:ncol,:pver,ixcldliq) = 0._r8
+             end where
+
+             ! also zero out number concentration
+             if ( microp_scheme .eq. 'MG' .or. microp_scheme .eq. 'M2M') then
+                where (state_q(:ncol,:pver,ixcldliq) < 1.e-12_r8)
+                   state_q(:ncol,:pver,ixnumliq) = 0._r8
+                end where
+             end if
+  #endif
+          else if (name == 'convect_deep') then
+             where (state_q(:ncol,:pver,ixcldliq) < 1.e-36_r8)
+                state_q(:ncol,:pver,ixcldliq) = 0._r8
+             end where
+             ! also zero out number concentration
+             if ( microp_scheme .eq. 'MG' .or. microp_scheme .eq. 'M2M' ) then
+                where (state_q(:ncol,:pver,ixcldliq) < 1.e-36_r8)
+                   state_q(:ncol,:pver,ixnumliq) = 0._r8
+                end where
+             end if
+          end if
+       end if
+    end if
+
+    ! special tests for cloud ice
+    if (ixcldice > 1) then
+       if(lq(ixcldice)) then
+          if (name == 'stratiform' .or. name == 'cldwat'  ) then
+  #ifdef PERGRO
+             where (state_q(:ncol,:pver,ixcldice) < 1.e-12_r8)
+                state_q(:ncol,:pver,ixcldice) = 0._r8
+             end where
+             ! also zero out number concentration
+             if ( microp_scheme .eq. 'MG' .or. microp_scheme .eq. 'M2M') then
+                where (state_q(:ncol,:pver,ixcldice) < 1.e-12_r8)
+                   state_q(:ncol,:pver,ixnumice) = 0._r8
+                end where
+             end if
+  #endif
+          else if (name == 'convect_deep') then
+             where (state_q(:ncol,:pver,ixcldice) < 1.e-36_r8)
+                state_q(:ncol,:pver,ixcldice) = 0._r8
+             end where
+             ! also zero out number concentration
+             if ( microp_scheme .eq. 'MG' .or. microp_scheme .eq. 'M2M' ) then
+                where (state_q(:ncol,:pver,ixcldice) < 1.e-36_r8)
+                   state_q(:ncol,:pver,ixnumice) = 0._r8
+                end where
+             end if
+          end if
+       end if
+    end if
+    
+    if (ls .or. lq(1)) then
+       call geopotential_dse(                                   &
+            lnpint, lnpmid, pint, pmid, pdel, rpdel,            &
+            state_s, state_q(1,1,1), phis, rair, gravit, cpair, &
+            zvir, t, zi, zm, ncol, fv_dycore)
+    end if
+    
+  end subroutine physics_update
+  
+  subroutine qneg3(ncol    ,ncold   ,lver    ,lconst_beg  , &
+                    lconst_end       ,qmin    ,q       )
+    
+    integer, intent(in) :: ncol         ! number of atmospheric columns
+    integer, intent(in) :: ncold        ! declared number of atmospheric columns
+    integer, intent(in) :: lver         ! number of vertical levels in column
+    integer, intent(in) :: lconst_beg   ! beginning constituent
+    integer, intent(in) :: lconst_end   ! ending    constituent
+
+    real(r8), intent(in) :: qmin(lconst_beg:lconst_end)      ! Global minimum constituent concentration
+
+ !
+ ! Input/Output arguments
+ !
+    real(r8), intent(inout) :: q(ncold,lver,lconst_beg:lconst_end) ! moisture/tracer field
+ !
+ !---------------------------Local workspace-----------------------------
+ !
+    integer indx(ncol,lver)  ! array of indices of points < qmin
+    integer nval(lver)       ! number of points < qmin for 1 level
+    integer nvals            ! number of values found < qmin
+    integer nn
+    integer iwtmp
+    integer i,ii,k           ! longitude, level indices
+    integer m                ! constituent index
+    integer iw,kw            ! i,k indices of worst violator
+
+    logical found            ! true => at least 1 minimum violator found
+
+    real(r8) worst           ! biggest violator
+ !
+ !-----------------------------------------------------------------------
+ !
+
+    do m=lconst_beg,lconst_end
+       nvals = 0
+       found = .false.
+       worst = 1.e35_r8
+       iw = -1
+ !
+ ! Test all field values for being less than minimum value. Set q = qmin
+ ! for all such points. Trace offenders and identify worst one.
+ !
+ !DIR$ preferstream
+       do k=1,lver
+          nval(k) = 0
+ !DIR$ prefervector
+          nn = 0
+          do i=1,ncol
+             if (q(i,k,m) < qmin(m)) then
+                nn = nn + 1
+                indx(nn,k) = i
+             end if
+          end do
+          nval(k) = nn
+       end do
+
+       do k=1,lver
+          if (nval(k) > 0) then
+             found = .true.
+             nvals = nvals + nval(k)
+             iwtmp = -1
+ !cdir nodep,altcode=loopcnt
+             do ii=1,nval(k)
+                i = indx(ii,k)
+                if (q(i,k,m) < worst) then
+                   worst = q(i,k,m)
+                   iwtmp = ii
+                end if
+             end do
+             if (iwtmp /= -1 ) kw = k
+             if (iwtmp /= -1 ) iw = indx(iwtmp,k)
+ !cdir nodep,altcode=loopcnt
+             do ii=1,nval(k)
+                i = indx(ii,k)
+                q(i,k,m) = qmin(m)
+             end do
+          end if
+       end do
+    end do
+ !
+    return
+  end subroutine qneg3
+  
+  subroutine geopotential_dse(                                &
+       piln   , pmln   , pint   , pmid   , pdel   , rpdel  ,  &
+       dse    , q      , phis   , rair   , gravit , cpair  ,  &
+       zvir   , t      , zi     , zm     , ncol   , fvdyn)
+!----------------------------------------------------------------------- 
+! 
+! Purpose: 
+! Compute the temperature  and geopotential height (above the surface) at the
+! midpoints and interfaces from the input dry static energy and pressures.
+!
+!-----------------------------------------------------------------------
+!------------------------------Arguments--------------------------------
+!
+! Input arguments
+    integer, intent(in) :: ncol                  ! Number of longitudes
+    logical, intent(in) :: fvdyn                 ! True if using finite volume dynamics
+
+    real(r8), intent(in) :: piln (pcols,pverp)   ! Log interface pressures
+    real(r8), intent(in) :: pmln (pcols,pver)    ! Log midpoint pressures
+    real(r8), intent(in) :: pint (pcols,pverp)   ! Interface pressures
+    real(r8), intent(in) :: pmid (pcols,pver)    ! Midpoint pressures
+    real(r8), intent(in) :: pdel (pcols,pver)    ! layer thickness
+    real(r8), intent(in) :: rpdel(pcols,pver)    ! inverse of layer thickness
+    real(r8), intent(in) :: dse  (pcols,pver)    ! dry static energy
+    real(r8), intent(in) :: q    (pcols,pver)    ! specific humidity
+    real(r8), intent(in) :: phis (pcols)         ! surface geopotential
+    real(r8), intent(in) :: rair                 ! Gas constant for dry air
+    real(r8), intent(in) :: gravit               ! Acceleration of gravity
+    real(r8), intent(in) :: cpair                ! specific heat at constant p for dry air
+    real(r8), intent(in) :: zvir                 ! rh2o/rair - 1
+
+! Output arguments
+
+    real(r8), intent(out) :: t(pcols,pver)       ! temperature
+    real(r8), intent(out) :: zi(pcols,pverp)     ! Height above surface at interfaces
+    real(r8), intent(out) :: zm(pcols,pver)      ! Geopotential height at mid level
+!
+!---------------------------Local variables-----------------------------
+!
+    integer  :: i,k                ! Lon, level, level indices
+    real(r8) :: hkk(pcols)         ! diagonal element of hydrostatic matrix
+    real(r8) :: hkl(pcols)         ! off-diagonal element
+    real(r8) :: rog                ! Rair / gravit
+    real(r8) :: tv                 ! virtual temperature
+    real(r8) :: tvfac              ! Tv/T
+!
+!-----------------------------------------------------------------------
+    rog = rair/gravit
+
+! The surface height is zero by definition.
+    do i = 1,ncol
+       zi(i,pverp) = 0.0_r8
+    end do
+
+! Compute the virtual temperature, zi, zm from bottom up
+! Note, zi(i,k) is the interface above zm(i,k)
+    do k = pver, 1, -1
+
+! First set hydrostatic elements consistent with dynamics
+       if (fvdyn) then
+          do i = 1,ncol
+             hkl(i) = piln(i,k+1) - piln(i,k)
+             hkk(i) = 1._r8 - pint(i,k) * hkl(i) * rpdel(i,k)
+          end do
+       else
+          do i = 1,ncol
+             hkl(i) = pdel(i,k) / pmid(i,k)
+             hkk(i) = 0.5_r8 * hkl(i)
+          end do
+       end if
+
+! Now compute tv, t, zm, zi
+       do i = 1,ncol
+          tvfac   = 1._r8 + zvir * q(i,k)
+          tv      = (dse(i,k) - phis(i) - gravit*zi(i,k+1)) / ((cpair / tvfac) + rair*hkk(i))
+
+          t (i,k) = tv / tvfac
+
+          zm(i,k) = zi(i,k+1) + rog * tv * hkk(i)
+          zi(i,k) = zi(i,k+1) + rog * tv * hkl(i)
+       end do
+    end do
+
+    return
+  end subroutine geopotential_dse
+  
+end module iap_state_update
+
+subroutine physics_update(state, tend, ptend, dt)
+!-----------------------------------------------------------------------
+! Update the state and or tendency structure with the parameterization tendencies
+!-----------------------------------------------------------------------
+  use geopotential, only: geopotential_dse
+  use constituents, only: cnst_get_ind
+  use scamMod,      only: scm_crm_mode, single_column
+  use phys_control, only: phys_getopts
+
+!------------------------------Arguments--------------------------------
+  type(physics_ptend), intent(inout)  :: ptend   ! Parameterization tendencies
+
+  type(physics_state), intent(inout)  :: state   ! Physics state variables
+  type(physics_tend ), intent(inout)  :: tend    ! Physics tendencies
+
+  real(r8), intent(in) :: dt                     ! time step
+!
+!---------------------------Local storage-------------------------------
+  integer :: i,k,m                               ! column,level,constituent indices
+  integer :: ixcldice, ixcldliq                  ! indices for CLDICE and CLDLIQ
+  integer :: ixnumice, ixnumliq
+  integer :: ncol                                ! number of columns
+  character*40 :: name    ! param and tracer name for qneg3
+
+  character(len=16) :: microp_scheme='RK'  ! microphysics scheme
+  !-----------------------------------------------------------------------
+
+  ! The column radiation model does not update the state
+  if(single_column.and.scm_crm_mode) return
+
+  call phys_getopts(microp_scheme_out=microp_scheme)
+
+  ncol = state%ncol
+
+  ! Update u,v fields
+  if(ptend%lu) then
+     do k = ptend%top_level, ptend%bot_level
+        do i = 1, ncol
+           state%u  (i,k) = state%u  (i,k) + ptend%u(i,k) * dt
+           tend%dudt(i,k) = tend%dudt(i,k) + ptend%u(i,k)
+        end do
+     end do
+  end if
+
+  if(ptend%lv) then
+     do k = ptend%top_level, ptend%bot_level
+        do i = 1, ncol
+           state%v  (i,k) = state%v  (i,k) + ptend%v(i,k) * dt
+           tend%dvdt(i,k) = tend%dvdt(i,k) + ptend%v(i,k)
+        end do
+     end do
+  end if
+
+  ! Update dry static energy
+  if(ptend%ls) then
+     do k = ptend%top_level, ptend%bot_level
+        do i = 1, ncol
+           state%s(i,k)   = state%s(i,k)   + ptend%s(i,k) * dt
+           tend%dtdt(i,k) = tend%dtdt(i,k) + ptend%s(i,k)/cpair
+        end do
+     end do
+  end if
+
+  ! Update constituents, all schemes use time split q: no tendency kept
+  call cnst_get_ind('CLDICE', ixcldice, abort=.false.)
+  call cnst_get_ind('CLDLIQ', ixcldliq, abort=.false.)
+  ! Check for number concentration of cloud liquid and cloud ice (if not present
+  ! the indices will be set to -1)
+  call cnst_get_ind('NUMICE', ixnumice, abort=.false.)
+  call cnst_get_ind('NUMLIQ', ixnumliq, abort=.false.)
+
+  do m = 1, pcnst
+     if(ptend%lq(m)) then
+        do k = ptend%top_level, ptend%bot_level
+           do i = 1,ncol
+              state%q(i,k,m) = state%q(i,k,m) + ptend%q(i,k,m) * dt
+           end do
+        end do
+
+        ! now test for mixing ratios which are too small
+        ! don't call qneg3 for number concentration variables
+        if (m .ne. ixnumice  .and.  m .ne. ixnumliq) then
+           name = trim(ptend%name) // '/' // trim(cnst_name(m))
+           call qneg3(trim(name), state%lchnk, ncol, pcols, pver, m, m, qmin(m), state%q(1,1,m))
+        else
+           do k = ptend%top_level, ptend%bot_level
+              do i = 1,ncol
+                 ! checks for number concentration
+                 state%q(i,k,m) = max(1.e-12_r8,state%q(i,k,m))
+                 state%q(i,k,m) = min(1.e10_r8,state%q(i,k,m))
+              end do
+           end do
+        end if
+
+     end if
+  end do
+
+  ! special tests for cloud liquid
+  if (ixcldliq > 1) then
+     if(ptend%lq(ixcldliq)) then
+        if (ptend%name == 'stratiform' .or. ptend%name == 'cldwat'  ) then
+#ifdef PERGRO
+           where (state%q(:ncol,:pver,ixcldliq) < 1.e-12_r8)
+              state%q(:ncol,:pver,ixcldliq) = 0._r8
+           end where
+
+           ! also zero out number concentration
+           if ( microp_scheme .eq. 'MG' .or. microp_scheme .eq. 'M2M') then
+              where (state%q(:ncol,:pver,ixcldliq) < 1.e-12_r8)
+                 state%q(:ncol,:pver,ixnumliq) = 0._r8
+              end where
+           end if
+#endif
+        else if (ptend%name == 'convect_deep') then
+           where (state%q(:ncol,:pver,ixcldliq) < 1.e-36_r8)
+              state%q(:ncol,:pver,ixcldliq) = 0._r8
+           end where
+           ! also zero out number concentration
+           if ( microp_scheme .eq. 'MG' .or. microp_scheme .eq. 'M2M' ) then
+              where (state%q(:ncol,:pver,ixcldliq) < 1.e-36_r8)
+                 state%q(:ncol,:pver,ixnumliq) = 0._r8
+              end where
+           end if
+        end if
+     end if
+  end if
+
+  ! special tests for cloud ice
+  if (ixcldice > 1) then
+     if(ptend%lq(ixcldice)) then
+        if (ptend%name == 'stratiform' .or. ptend%name == 'cldwat'  ) then
+#ifdef PERGRO
+           where (state%q(:ncol,:pver,ixcldice) < 1.e-12_r8)
+              state%q(:ncol,:pver,ixcldice) = 0._r8
+           end where
+           ! also zero out number concentration
+           if ( microp_scheme .eq. 'MG' .or. microp_scheme .eq. 'M2M') then
+              where (state%q(:ncol,:pver,ixcldice) < 1.e-12_r8)
+                 state%q(:ncol,:pver,ixnumice) = 0._r8
+              end where
+           end if
+#endif
+        else if (ptend%name == 'convect_deep') then
+           where (state%q(:ncol,:pver,ixcldice) < 1.e-36_r8)
+              state%q(:ncol,:pver,ixcldice) = 0._r8
+           end where
+           ! also zero out number concentration
+           if ( microp_scheme .eq. 'MG' .or. microp_scheme .eq. 'M2M' ) then
+              where (state%q(:ncol,:pver,ixcldice) < 1.e-36_r8)
+                 state%q(:ncol,:pver,ixnumice) = 0._r8
+              end where
+           end if
+        end if
+     end if
+  end if
+
+  ! Derive new temperature and geopotential fields if heating or water tendency not 0.
+  if (ptend%ls .or. ptend%lq(1)) then
+     call geopotential_dse(                                                                    &
+          state%lnpint, state%lnpmid, state%pint  , state%pmid  , state%pdel  , state%rpdel  , &
+          state%s     , state%q(1,1,1),state%phis , rair        , gravit      , cpair        , &
+          zvir        , state%t     , state%zi    , state%zm    , ncol         )
+  end if
+
+     ! juanxiong he
+#ifdef CO2
+     do i=1,ncol
+     do k=1,pver
+     if(isnan(state%zm(i,k))) print *,'s=',i,k,state%s(i,k)
+     end do
+     end do
+#endif
+
+  ! Reset all parameterization tendency flags to false
+  call physics_ptend_reset(ptend)
+
+end subroutine physics_update
---
+++ physics/zm_convr_post.F90
@@ -0,0 +1,92 @@
+module zm_convr_post
+  
+  use shr_kind_mod,    only: r8 => shr_kind_r8
+  
+  implicit none
+  
+  contains
+  
+  subroutine zm_convr_post_run(ncol, pcols, pver, pcnst, lengath, ixcldice, ixcldliq, ixnumice, ixnumliq, gravit, rair, cpair, zvir, dt, jt, maxg, ideep,   &
+    fv_dycore, microp_scheme, qmin, ps, lnpint, lnpmid, pint, pmid, pdel, rpdel, phis, tend_q, tend_s, state_u, state_v, state_s, state_q, mcon, pcont, pconb, dp_cldliq, dp_cldice, &
+    temp_state_u, temp_state_v, temp_state_s, temp_state_q, temp_state_t, temp_state_zm, temp_state_zi, errmsg, errflg)
+    
+    use iap_state_update, only :: physics_update
+    
+    integer,           intent(in   )                   :: ncol, pcols, pver, pcnst, lengath, &
+                                                          ixcldice, ixcldliq, ixnumice, ixnumliq
+    integer,           intent(in   ), dimension(:)     :: jt, maxg, ideep
+    logical,           intent(in   )                   :: fv_dycore
+    character(len=16), intent(in   )                   :: microp_scheme
+    real(kind=r8),     intent(in   )                   :: gravit, rair, cpair, zvir, dt
+    real(kind=r8),     intent(in   ), dimension(:)     :: qmin
+    real(kind=r8),     intent(in   ), dimension(:)     :: ps, phis
+    real(kind=r8),     intent(in   ), dimension(:,:)   :: lnpint, pint
+    real(kind=r8),     intent(in   ), dimension(:,:)   :: lnpmid, pmid, pdel, rpdel, tend_q, tend_s
+    real(kind=r8),     intent(in   ), dimension(:,:)   :: state_u, state_v, state_s
+    real(kind=r8),     intent(in   ), dimension(:,:,:) :: state_q
+    real(kind=r8),     intent(inout), dimension(:,:)   :: mcon
+    real(kind=r8),     intent(  out), dimension(:)     :: pcont, pconb
+    real(kind=r8),     intent(  out), dimension(:,:)   :: dp_cldliq, dp_cldice
+    real(kind=r8),     intent(  out), dimension(:,:)   :: temp_state_u, temp_state_v, temp_state_s
+    real(kind=r8),     intent(  out), dimension(:,:,:) :: temp_state_q
+    real(kind=r8),     intent(  out), dimension(:,:)   :: temp_state_t, temp_state_zm, temp_state_zi
+    
+    ! CCPP error handling
+    character(len=*),          intent(  out) :: errmsg
+    integer,                   intent(  out) :: errflg
+    
+    integer :: i, top, bot
+    logical :: lu, lv, ls
+    logical, dimension(pcnst) :: lq
+    character*24 :: name
+    real(kind=r8), dimension(pcols,pver) :: tend_u, tend_v
+    
+    ! Initialize the CCPP error handling variables
+    errmsg = ''
+    errflg = 0
+    
+    !
+    ! Convert mass flux from reported mb/s to kg/m^2/s
+    !
+    mcon(:ncol,:pver) = mcon(:ncol,:pver) * 100._r8/gravit
+    
+    pcont(:ncol) = ps(:ncol)
+    pconb(:ncol) = ps(:ncol)
+    do i = 1,lengath
+      if (maxg(i).gt.jt(i)) then
+        pcont(ideep(i)) = pmid(ideep(i),jt(i))  ! gathered array (or jctop ungathered)
+        pconb(ideep(i)) = pmid(ideep(i),maxg(i))! gathered array
+      endif
+    end do
+    
+    ! update temporary physics state variables (for use in subsequent zm_conv routines) with tendencies coming from zm_convr 
+    !t, zm, zi - from state1 var; need new vars in phys_int_ephem to hold
+    lu = .false.
+    lv = .false.
+    ls = .true.
+    lq(1) = .true.
+    lq(2:) = .false.
+    name = 'zm_convr'
+    top = 1
+    bot = pver
+    tend_u = 0._r8
+    tend_v = 0._r8
+    
+    !GJF: replaces the physics_state_copy(state,state1) call in zm_conv_intr.F90/zm_conv_tend
+    temp_state_u(:,:) = state_u(:,:)
+    temp_state_v(:,:) = state_v(:,:)
+    temp_state_s(:,:) = state_s(:,:)
+    temp_state_q(:,:,:) = state_q(:,:,:)
+    
+    call physics_update(lu, lv, ls, lq, fv_dycore, name, microp_scheme, top, bot, ncol, &
+                        pcols, pver, pcnst, ixcldice, ixcldliq, ixnumice, ixnumliq,     &
+                        rair, gravit, cpair, zvir, dt, qmin, tend_u, tend_v, tend_s, tend_q, &
+                        lnpint, lnpmid, pint, pmid, pdel, rpdel, phis, temp_state_u, &
+                        temp_state_v, temp_state_s, temp_state_q, temp_state_t, temp_state_zm, &
+                        temp_state_zi)
+    
+    dp_cldliq(:ncol,:) = 0._r8
+    dp_cldice(:ncol,:) = 0._r8
+    
+  end subroutine zm_convr_post_run
+end module zm_convr_post
