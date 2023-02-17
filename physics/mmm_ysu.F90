c4abdadd4adb1682400163abbf85ff42f3c22689
dustinswales <dswales@ucar.edu>
2023-02-17 16:37:56 -0700

    Nearly complete with MMM YSU BL porting.

--- physics/mmm_ysu.F90
+++ physics/mmm_ysu.F90
@@ -3,48 +3,89 @@ module mmm_ysu
   use bl_ysu,  only: bl_ysu_run
   implicit none
 
-  public mmm_ysu_run
+  ! Module constants
+  real(kind_phys) :: rovcp, rovg
+
+  public mmm_ysu_init, mmm_ysu_run
 
 contains
+
+!> \section arg_table_mmm_ysu_init
+!! \htmlinclude mmm_ysu_init.html
+!!
+  ! #########################################################################################
+  ! #########################################################################################
+  subroutine mmm_ysu_init(con_rd, con_cp, con_g, errmsg, errflg)
+    ! Inputs
+    real(kind_phys),intent(in) :: con_cp, con_g, con_rd
+
+    ! Outputs
+    character(len=*), intent(out) :: errmsg
+    integer,          intent(out) :: errflg
+
+    ! Initialize CCPP error handling
+    errmsg = ''
+    errflg = 0
+
+    ! Compute some constants.
+    rovcp = con_rd/con_cp
+    rovg  = con_rd/con_g
+
+  end subroutine mmm_ysu_init
 
 !> \section arg_table_mmm_ysu_run
 !! \htmlinclude mmm_ysu_run.html
 !!
   ! #########################################################################################
   ! #########################################################################################
-  subroutine mmm_ysu_run(nCol, nLay, nmix, slimask, u, v, t, qx, ntcw, ntiw, p2d, p2di, phii, pi2d, &
-       ps, zorl, psim, psih, heat, evap, sfc_tau, wspd, u10, v10, cp, g, rd, ep1, ep2, br, karman, dtp, xlv, rv, &
-       xmu, lwh, swh, do_ysu_cldliq, do_ysu_cldice, dudt_pbl, dvdt_pbl, dtdt_pbl, dqvdt_pbl, dqcdt_pbl, dqidt_pbl, dqtdt_pbl, exch_hx, exch_mx, hpbl, kpbl1d, wstar, delta, errmsg, errflg)
+  subroutine mmm_ysu_run(nCol, nLay, ntrac, slimask, u, v, t, q, ntcw, ntiw, p, pi, phii,   &
+       prslk, ps, zorl, psim, psih, heat, evap, sfc_tau, wspd, u10, v10, con_cp, con_g,     &
+       con_rd, ep1, ep2, br, karman, dtp, xlv, con_rv, xmu, lwh, swh, do_ysu_cldliq,        &
+       do_ysu_cldice, ysu_add_bep, ysu_topdown_pblmix, uo_sfc, vo_sfc, ctopo, ctopo2,       &
+       frcurb, a_u, a_v, a_t, a_q, a_e, b_u, b_v, b_t, b_q, b_e, dlu, dlg, sfk, vlk,        &
+       dudt_pbl, dvdt_pbl, dtdt_pbl, dqvdt_pbl, dqcdt_pbl, dqidt_pbl, dqtdt_pbl, exch_hx,   &
+       exch_mx, hpbl, kpbl1d, wstar, delta, errmsg, errflg)
+
     ! Inputs
-    logical, intent(in) :: do_ysu_cldliq, do_ysu_cldice
-    integer, intent(in) :: nCol, nLay, nmix, ntcw, ntiw
+    logical, intent(in) :: do_ysu_cldliq, do_ysu_cldice, ysu_add_bep, ysu_topdown_pblmix
+    integer, intent(in) :: nCol, nLay, ntrac, ntcw, ntiw
     integer, intent(in),dimension(:) :: slimask
-    real(kind_phys),intent(in) :: cp, g, rd, ep1, ep2, karman, rv, xlv, dtp
-    real(kind_phys),intent(in),dimension(:) :: ps, zorl, psim, psih, heat, evap, sfc_tau, wspd, br, xmu
-    real(kind_phys),intent(in),dimension(:,:) :: u, v, t, p2d, p2di, pi2d, swh, lwh, phii
-    real(kind_phys),intent(in),dimension(:,:,:) :: qx
+    real(kind_phys),intent(in) :: con_cp, con_g, con_rd, ep1, ep2, karman, con_rv, xlv, dtp
+    real(kind_phys),intent(in),dimension(:) :: ps, zorl, psim, psih, heat, evap, sfc_tau,   &
+         wspd, br, xmu
+    real(kind_phys),intent(in),dimension(:,:) :: u, v, t, p, pi, prslk, swh, lwh, phii
+    real(kind_phys),intent(in),dimension(:,:,:) :: q
+    real(kind_phys),intent(in),dimension(:), optional :: uo_sfc, vo_sfc, ctopo, ctopo2,     &
+         frcurb
+    real(kind_phys),intent(in),dimension(:,:), optional :: a_u, a_v, a_t, a_q, a_e, b_u,    &
+         b_v, b_t, b_q, b_e, dlu, dlg, sfk, vlk ! DJS: These still need metadata
     ! Outputs
     character(len=*), intent(out) :: errmsg
     integer,          intent(out) :: errflg
     integer,          intent(inout), dimension(:) :: kpbl1d
-    
-    real(kind_phys),  intent(inout), dimension(:) :: u10, v10, hpbl, wstar, delta, exch_hx, exch_mx
-    real(kind_phys),  intent(inout), dimension(:,:) :: dudt_pbl, dvdt_pbl, dtdt_pbl, dqvdt_pbl, dqcdt_pbl, dqidt_pbl
+    real(kind_phys),  intent(inout), dimension(:) :: u10, v10, hpbl, wstar, delta, exch_hx, &
+         exch_mx
+    real(kind_phys),  intent(inout), dimension(:,:) :: dudt_pbl, dvdt_pbl, dtdt_pbl,        &
+         dqvdt_pbl, dqcdt_pbl, dqidt_pbl
     real(kind_phys),  intent(inout), dimension(:,:,:) :: dqtdt_pbl
 
     ! Locals
-    integer :: iCol, iLay
-    real :: tvcon, rovcp, rovg
-    real(kind_phys),dimension(nCol) :: xland, hfx, qfx, rhox, govrth, ust, znt, uox, vox, ctopo, ctopo2, frcurb
-    real(kind_phys),dimension(nCol, nLay) :: thx, thlix, thvx, rthraten, dz8w2d, a_u, a_v, a_t, a_q, a_e, b_u, b_v, b_t, b_q, b_e, dlu, dlg, sfk, vlk
-    integer :: ysu_topdown_pblmix = 1
-    logical :: flag_bep = .true.
+    integer :: iCol, iLay, ysu_topdown_pblmix_int
+    real(kind_phys) :: tvcon
+    real(kind_phys),dimension(nCol) :: xland, hfx, qfx, rho, govrth, ust, znt
+    real(kind_phys),dimension(nCol, nLay) :: theta, rthraten
+    real(kind_phys),dimension(nCol, nLay+1) :: zi
 
-    ! Compute constants
-    rovcp = rd/cp
-    rovg  = rd/g
+    ! Initialize CCPP error handling
+    errmsg = ''
+    errflg = 0
 
-    ! Compute land/sea mask for ysu
+    ! #######################################################################################
+    ! GFS MMM-YSU-PBL pre
+    ! Compute inputs for YSU scheme...
+    ! #######################################################################################
+
+    ! Compute land/sea mask convention for YSU from (0-sea/1-land/2-ice) ---> (1-land/2-sea)
     do iCol=1,nCol
        if(slimask(iCol).eq.0) then
           xland(iCol) = 2
@@ -53,93 +94,58 @@ contains
        end if
     end do
 
-    do iLay = 1,nLay
+    ! Height (m) at interface from geopotential (m2/s2)
+    do iLay = 1,nLay+1
        do iCol = 1,nCol
-          thx(iCol,iLay)   = t(iCol,iLay)/pi2d(iCol,iLay)
-          thlix(iCol,iLay) = (t(iCol,iLay)-xlv*qx(iCol,iLay,ntcw)/cp-2.834E6*qx(iCol,iLay,ntiw)/cp)/pi2d(iCol,iLay)
+          zi(iCol,iLay) = phii(iCol,iLay)/con_g
        enddo
     enddo
+
+    ! Potential temperature (K)
+    do iLay = 1,nLay
+       do iCol = 1,nCol
+          theta(iCol,iLay)   = t(iCol,iLay)/prslk(iCol,iLay)
+       enddo
+    enddo
+    govrth(:) = con_g/theta(:,1)   ! gravity divided by theta at the surface
     
+    ! Total (longwave+shortwave) radiative heating rate from temp/s -> theta/s
     do iLay = 1,nLay
        do iCol = 1,nCol
-          tvcon = (1.+ep1*qx(iCol,iLay,1))
-          thvx(iCol,iLay) = thx(iCol,iLay)*tvcon
+          rthraten(iCol,iLay) = (swh(iCol,iLay)*xmu(iCol)+lwh(iCol,iLay))/prslk(iCol,iLay)
        enddo
     enddo
 
-    ! Convert total radiative heating rate from temp/s -> theta/s
-    do iLay = 1,nLay
-       do iCol = 1,nCol
-          rthraten(iCol,iLay) = (swh(iCol,iLay)*xmu(iCol)+lwh(iCol,iLay))/pi2d(iCol,iLay)
-       enddo
-    enddo
-
+    ! Kinematic surface fluxes
     do iCol = 1,nCol
-       tvcon = (1.+ep1*qx(iCol,1,1))
-       rhox(iCol) = ps(iCol)/(rd*t(iCol,1)*tvcon)
-       govrth(iCol) = g/thx(iCol,1)
-       hfx(iCol) = heat(iCol)*rhox(iCol)*cp
-       qfx(iCol) = evap(iCol)*rhox(iCol)
-       ust(iCol) = sqrt(sfc_tau(iCol))
-       znt(iCol) = 0.01*zorl(iCol)
-       uox(iCol) = 0.0
-       vox(iCol) = 0.0
-       ctopo(iCol) = 0.0
-       ctopo2(iCol) = 0.0
-       a_u(iCol,:) = 0.0
-       a_v(iCol,:) = 0.0
-       a_t(iCol,:) = 0.0
-       a_q(iCol,:) = 0.0
-       a_e(iCol,:) = 0.0
-       b_u(iCol,:) = 0.0
-       b_v(iCol,:) = 0.0
-       b_t(iCol,:) = 0.0
-       b_q(iCol,:) = 0.0
-       b_e(iCol,:) = 0.0
-       dlu(iCol,:) = 0.0
-       dlg(iCol,:) = 0.0
-       sfk(iCol,:) = 0.0
-       vlk(iCol,:) = 0.0
-       frcurb(iCol) = 0.0
+       tvcon     = (1.+ep1*q(iCol,1,1))
+       rho(iCol) = ps(iCol)/(con_rd*t(iCol,1)*tvcon)
+       hfx(iCol) = heat(iCol)*rho(iCol)*con_cp
+       qfx(iCol) = evap(iCol)*rho(iCol)
     enddo
 
-    ! Convert height from geopotential
-    do iLay = 1,nLay
-       do iCol = 1,nCol
-          dz8w2d(iCol,iLay) = phii(iCol,iLay)/g
-       enddo
-    enddo
+    ! Surface roughness length (cm) -> (meters)
+    znt(:) = 0.01*zorl(:)
 
-  ! #########################################################################################
-    call bl_ysu_run(u, v, t, qx(:,:,1), qx(:,:,ntcw), qx(:,:,ntiw), nmix, qx, p2d, p2di, &
-         pi2d, do_ysu_cldliq, do_ysu_cldice, dudt_pbl, dvdt_pbl, dtdt_pbl, dqvdt_pbl,       &
-         dqcdt_pbl, dqidt_pbl, dqtdt_pbl, cp, g, rovcp, rd, rovg, ep1, ep2, karman, xlv, rv,&
-         dz8w2d, psfcpa, znt, ust, hpbl, psim, psih, xland, hfx, qfx, wspd, br, dtp, &
-         kpbl1d, exch_hx, exch_mx, wstar, delta, u10, v10, &
-         uox, &! Optional
-         vox, & ! Optional
-         rthraten, &
-         ysu_topdown_pblmix, &
-         ctopo, & ! Optional 
-         ctopo2, & ! Optional 
-         a_u, & ! Optional
-         a_v, & ! Optional 
-         a_t, & ! Optional 
-         a_q, & ! Optional 
-         a_e, & ! Optional 
-         b_u, & ! Optional 
-         b_v, & ! Optional 
-         b_t, & ! Optional 
-         b_q, & ! Optional 
-         b_e, & ! Optional 
-         sfk, & ! Optional 
-         vlk, & ! Optional 
-         dlu, & ! Optional 
-         dlg, & ! Optional 
-         frcurb, & ! Optional 
-         flag_bep, &
-         1, nCol, nLay, nLay+1, errmsg, errflg)
-    
+    ! Surface friction velocity (from surface wind-stress)
+    ust(:) = sqrt(sfc_tau(:))
+
+    ! YSU scheme needs 0/1 flag, we have .true./.false.
+    ysu_topdown_pblmix_int = 0
+    if (ysu_topdown_pblmix) ysu_topdown_pblmix_int = 1
+
+    ! #######################################################################################
+    !
+    ! Call MMM-YSU-PBL scheme
+    !
+    ! #######################################################################################
+    call bl_ysu_run(u, v, t, q(:,:,1), q(:,:,ntcw), q(:,:,ntiw), ntrac, q, p, pi, prslk,    &
+         do_ysu_cldliq, do_ysu_cldice, dudt_pbl, dvdt_pbl, dtdt_pbl, dqvdt_pbl, dqcdt_pbl,  &
+         dqidt_pbl, dqtdt_pbl, con_cp, con_g, rovcp, con_rd, rovg, ep1, ep2, karman, xlv,   &
+         con_rv, zi, ps, znt, ust, hpbl, psim, psih, xland, hfx, qfx, wspd, br, dtp,        &
+         kpbl1d, exch_hx, exch_mx, wstar, delta, u10, v10, uo_sfc, vo_sfc, rthraten,        &
+         ysu_topdown_pblmix_int, ctopo, ctopo2, a_u, a_v, a_t, a_q, a_e, b_u, b_v, b_t, b_q,&
+         b_e, sfk, vlk, dlu, dlg, frcurb, ysu_add_bep, 1, nCol, nLay, nLay+1, errmsg, errflg)
 
   end subroutine mmm_ysu_run
 end module mmm_ysu
