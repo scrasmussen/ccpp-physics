8a9f0d49d5c8cd125b1cebcf70af06b248a2219f
Soren Rasmussen <s.c.rasmussen@gmail.com>
2024-03-27 13:25:22 -0600

    mmm ntiedtke: add ntiedtke submodule

---
+++ physics/hooks/mmm_kinds.F90
@@ -0,0 +1,9 @@
+module ccpp_kinds
+  use machine,  only: kind_phys, kind_dbl_prec, kind_sngl_prec
+  implicit none
+end module ccpp_kinds
+
+module ccpp_kind_types
+  use machine,  only: kind_phys, kind_dbl_prec, kind_sngl_prec
+  implicit none
+end module ccpp_kind_types
